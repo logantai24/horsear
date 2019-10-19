2342398fe8131b3b2fa0acb4d597e1fd85fa3149
Logan Tai <puihangtai@PUIde-MacBook-Pro.local>
2019-10-19 14:23:02 +0800

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+$( document ).ready(function(){
+    // $('.carousel').carousel();
+    $('.next').click(function(){ $('#mycarousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('#mycarousel').carousel('prev');return false; });
+});
+
+
+    
+
+
+
+    
